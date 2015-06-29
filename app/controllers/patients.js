48025b70bd42aab860fda8e85618fd57a994974c
David Tresner-Kirsch <davidtk@mitre.org>
2015-06-29 17:09:21 -0400

    create patient form

--- app/controllers/patients.js
+++ app/controllers/patients.js
@@ -1,6 +1,30 @@
 import Ember from 'ember';
 
 var PatientsController = Ember.ArrayController.extend({
+  actions:  {
+    createPatient: function() {
+      var givenName = this.get('givenName');
+      var familyName = this.get('familyName');
+      if (!givenName.trim() || !familyName.trim()) { return; }
+
+      var name = this.store.createRecord('patient', {
+	  given: givenName,
+	  family: familyName
+      });
+      var patient = this.store.createRecord('patient', {
+	  names: [name],
+	  birthDate: new Date()
+      });
+
+      // Clear the text fields
+      // this.set('familyName', '');
+      // this.set('givenName', '');
+
+      // Save the new model
+      patient.save();
+    }
+  },
+
   filterText: '',
 
   filteredContent: function(){
