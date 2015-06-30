7eb5ec647e532633b18e728cac0b690549c29e6d
ccourtney <ccourtney@mitre.org>
2015-06-29 22:46:58 -0400

    Various aesthetic and functional fixes.

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
     isBannerDisplayed: true,
     actions:{
-      toggleViewVisibitity:function(){
+      toggleViewVisibility:function(){
           //alert('toggleViewVisibility called!');
           this.set('isBannerDisplayed',false);
          // this.set('isBannerDisplayed', !this.get('isBannerDisplayed'));
