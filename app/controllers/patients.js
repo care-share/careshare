02e39bb8f66ec415b423e035f9b0d84a3c97e9d4
ccourtney <ccourtney@mitre.org>
2015-07-07 12:40:28 -0400

    Minor cleanup

--- app/controllers/patients.js
+++ app/controllers/patients.js
@@ -6,12 +6,9 @@ var PatientsController = Ember.ArrayController.extend({
   filteredContent: function(){
     var filter = this.get('filterText');
     var rx = new RegExp(filter, 'gi');
-    console.log("filterText:"+filter);
     var patients = this.get('content');
-    console.log("patients: '"+patients+"'");
 
     return patients.filter(function(patient) {
-        console.log('patient:'+patient.get('fullName'));
       return patient.get('fullName').toString().match(rx);
     });
 
