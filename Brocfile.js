1e438dbc0910cabb978aad03990744ba951f81f8
ccourtney <ccourtney@mitre.org>
2015-07-15 13:05:21 -0400

    Care share app now almost completely mimics the RoR app styles and layout.

--- Brocfile.js
+++ Brocfile.js
@@ -5,6 +5,8 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var app = new EmberApp();
 
 app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
+//app.import('bower_components/ionicons/css/ionicons.css');
+//app.import('bower_components/ionicons/css/ionicons.min.css');
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
