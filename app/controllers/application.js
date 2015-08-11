5fd2147a7006dd225dab1e5ae224d9610beb3845
ccourtney <ccourtney@mitre.org>
2015-08-11 10:58:55 -0400

    Basic account request stub is now accessible from main page.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,12 +3,17 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     isSideBarDisplayed: true,
     lastLoginFailed: false,
+    isShowingForm: true,
     role: 'user',
     patientCounter: 0,
     isAdmin: function() {
         return this.get('session').content.secure.user.role === 'admin';
     }.property('role'),
     actions:{
+      accountRequest:function(){
+      var info = this.getProperties('fullName','email');
+        alert('Account request sent: Full Name - '+info.fullName+',Email - '+info.email);
+      },
       toggleSideBarVisibility:function(){
         this.set('isSideBarDisplayed',false);
       },
@@ -27,5 +32,9 @@ export default Ember.Controller.extend({
         console.log('getPatientCount called!');
         return 5;
       }.property('model', 'patientCounter')
+      ,
+    toggleLoginForm: function(){
+      this.toggleProperty('isShowingForm');
+    }
     }
 });
