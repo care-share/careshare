a586d5084024fc1e4015ea3863210d9484d422f9
ccourtney <ccourtney@mitre.org>
2015-09-14 14:50:46 -0400

    Various small bug fixes to fhir-edit.

--- app/components/fhir-edit.js
+++ app/components/fhir-edit.js
@@ -2,33 +2,29 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isEditing:false,
-  editable: null,
-  field: null,
-  value: '',
+  originalValue: '',
   actions:{
-    editItem: function(element,attribute,name) {
-      console.log(JSON.stringify(element)+','+name);
-      this.set('editable',element);
-      this.set('field',attribute);
-      this.set('value',name);
+    editItem: function() {
+      console.log('editItem');
+      this.set('originalValue',this.get('attribute'));
       this.set('isEditing',true);
     },
     cancel: function(){
       console.log('cancel');
+      this.set('element.'+this.get('name'),this.get('originalValue'));
       this.set('isEditing',false);
     },
-    saveItem: function(key){
-      if(key.length > 0){
-        console.log(this.get('field')+" request change to: "+key);
-        this.set('field',key);
-        this.get('editable').save();
+    saveItem: function(){
+      console.log('saveItem');
+      if(this.get('attribute').length > 0){
+        this.get('element').save();
       }
       this.set('isEditing',false);
     },
     removeItem: function(){
-      console.log('removeItem called with: '+this.get('value'));
-      this.set('editable.'+this.get('value'),'');
-      this.get('editable').save();
+      console.log('removeItem');
+      this.set('element.'+this.get('name'),'');
+      this.get('element').save();
       this.set('isEditing',false);
     }
   }
