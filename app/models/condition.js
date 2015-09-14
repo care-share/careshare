c75675852416eff859a93b35d32e076909e0af7e
David Tresner-Kirsch <davidtk@mitre.org>
2015-09-14 09:50:01 -0400

    let the patient model find associated conditions, and compute a display name property for conditions

---
+++ app/models/condition.js
@@ -0,0 +1,16 @@
+import condition from 'ember-fhir-adapter/models/condition';
+
+
+export default condition.extend({ 
+
+    displayName: Ember.computed("name", function(){
+	// There might be multiple codings, but we only want one
+	// human readable display name, so we just take it from the
+	// first coding we find. If the business need changes, this
+	// is where to implement the change.
+	return this.get("code.coding.firstObject.display");
+    }),
+
+    conditions: DS.hasMany('condition', {"async": true})
+
+});
--- app/serializers/patient.js
+++ app/serializers/patient.js
@@ -3,6 +3,18 @@ import ApplicationSerializer from './application';
 export default ApplicationSerializer.extend({
   attrs: {
     name : {embedded: 'always'}
+  },
+  normalize: function(type, hash, prop){
+    let queryParam = `?patient:Patient=${hash.id}`;
+    (hash.content||hash)["links"] = {
+      conditions: `/Condition${queryParam}`,
+      observations: `/Observation${queryParam}`,
+      encounters: `/Encounter${queryParam}`,
+      medications: `/MedicationStatement${queryParam}`,
+      appointments: `/Appointment${queryParam}`,
+      risks: `/RiskAssessment?subject:Patient=${hash.id}`
+    };
+    return this._super(type, hash, prop);
   }
 
 });
