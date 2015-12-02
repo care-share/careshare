f52bcbfb1a4a158e40a481787f4e5ba484630672
Joe Portner <jportner@mitre.org>
2015-12-02 12:06:37 -0500

    Fixed FHIR choice not setting properly

--- app/components/fhir-choice.js
+++ app/components/fhir-choice.js
@@ -1,35 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    isEditing: false,
-    tagName: 'span',    
-    classNames: ['fhir-choice'],
-    originalValue: '',
     finalChoices: [],
     actions: {
-        editItem: function () {
-            console.log('editItem');
-            this.set('originalValue', this.get('attribute'));
-            this.set('isEditing', true);
-        },
-        cancel: function () {
-            console.log('cancel');
-            this.set('attribute', this.get('originalValue'));
-            this.set('isEditing', false);
-        },
-        saveItem: function () {
-            console.log('saveItem');
-            this.set('isEditing', false);
-        },
-        setChoice: function (choice) {
-            this.set('attribute', choice);
+        saveItem: function (choice) {
+            this.set('attribute', choice.target.selectedOptions[0].value);
         }
     },
     onInitialization: function () {
         this.set('finalChoices', this.get('choices').split(','));
-        //$('.selectpicker').selectpicker();
-    }.on('init'),
-    didInsertElement: function(){
-        $(".selectpicker").selectpicker()
-    }
+    }.on('init')
 });
