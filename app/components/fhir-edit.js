5b728855cd06eee2a2ce9613b86599045343f995
Colin Courtney <ccourtney@mitre.org>
2015-12-15 17:04:40 -0500

    Patch appears beside edited fhir-edit field, with styling.

--- app/components/fhir-edit.js
+++ app/components/fhir-edit.js
@@ -4,6 +4,7 @@ export default Ember.Component.extend({
     tagName: 'span',
 	original: null,
 	patcher: null,
+	calculatedPatch: '',
     classNames: ['fhir-edit'],
     setup: function () {
 	    this.set('original',this.get('attribute'));
@@ -14,7 +15,16 @@ export default Ember.Component.extend({
 	    var patcher = this.get('patcher');
 	    var diff = patcher.diff_main(this.get('original'),this.get('attribute'),true);
 		var patch_list = patcher.patch_make(this.get('original'),this.get('attribute'),diff);
-        console.log('Typed: ' + this.get('attribute'));
-		console.log('Diff: ' + patcher.patch_toText(patch_list));
+		var finalPatch = '';
+		patcher.patch_toText(patch_list).split('\n').forEach(function(patch){
+		    if(patch.length > 0 && patch.slice(0, '@@ '.length) !== '@@ '){
+			    if(patch.slice(0,1) === '+'){
+		        finalPatch += '<span style=\'background-color:#d4e2c1\'>'+patch.slice(1)+'</span>';
+				}else if(patch.slice(0,1) === '-'){
+				    finalPatch += '<s style=\'background-color:#fadee8\'>'+patch.slice(1)+'</s>';
+				}else{finalPatch += patch.trim();}
+			}
+		});
+		this.set('calculatedPatch',finalPatch);
     }.observes('attribute')
 });
