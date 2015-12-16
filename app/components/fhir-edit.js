d8a2baefa19baa748ff16c85922ff766a3c1b850
Colin Courtney <ccourtney@mitre.org>
2015-12-16 12:59:05 -0500

    Fixed bug where removing first letter sometimes removed last letter erroneously; simplified diff code.

--- app/components/fhir-edit.js
+++ app/components/fhir-edit.js
@@ -14,18 +14,7 @@ export default Ember.Component.extend({
 	attrObserve: function () {
 	    var patcher = this.get('patcher');
 	    var diff = patcher.diff_main(this.get('original'),this.get('attribute'),true);
-		var patch_list = patcher.patch_make(this.get('original'),this.get('attribute'),diff);
-		var finalPatch = '';
-		patcher.patch_toText(patch_list).split('\n').forEach(function(patch){
-		    if(patch.length > 0 && patch.slice(0, '@@ '.length) !== '@@ '){
-			    if(patch.slice(0,1) === '+'){
-		        finalPatch += '<span style=\'background-color:#d4e2c1\'>'+patch.slice(1)+'</span>';
-				}else if(patch.slice(0,1) === '-'){
-				    finalPatch += '<s style=\'background-color:#fadee8\'>'+patch.slice(1)+'</s>';
-				}else{finalPatch += patch.trim();}
-			}
-		});
-		this.set('calculatedPatch',finalPatch);
+		this.set('calculatedPatch',patcher.diff_prettyHtml(diff));
     }.observes('attribute'),
 	actions:{
 	    cancel: function(){
