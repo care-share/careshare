1e018af0ad8a69f46431f430d4633aae7c166c68
Colin Courtney <ccourtney@mitre.org>
2015-12-30 14:56:05 -0500

    Fhir-edit now writes changes to parent attribute+'Diff'.

--- app/components/fhir-edit.js
+++ app/components/fhir-edit.js
@@ -9,14 +9,21 @@ export default Ember.Component.extend({
     classNames: ['fhir-edit'],
     setup: function () {
 	    this.set('patcher',new diff_match_patch());
-	    this.set('original',this.get('parent.' + this.get('name')));
-		this.set('diffAttribute',this.get('parent.' + this.get('name') + 'Diff'));
-        console.log('INIT: FHIR-EDIT- name: ' + this.get('name'));	
+	    this.set('original',this.get('parent').get(this.get('name')));
+		console.log('(FHIR-EDIT) parent: '+this.get('parent')+', name: '+this.get('name')
+		    +', original: '+this.get('original')+', diffAttribute is named: parent.'+this.get('name')+'Diff');
+		this.set('diffAttribute',this.get('parent.' + this.get('name') + 'Diff'));	
     }.on('init'),
 	attrObserve: function () {
-	    var patcher = this.get('patcher');
-	    var diff = patcher.diff_main(this.get('original'),this.get('diffAttribute'),true);
-		this.set('calculatedPatch',patcher.diff_prettyHtml(diff));
+	    console.log('(FHIR-EDIT) diffAttribute altered, diffAttribute is: '+this.get('diffAttribute'));
+		if(this.get('diffAttribute') !== null && this.get('diffAttribute') !== undefined){
+			var patcher = this.get('patcher');
+			var diff = patcher.diff_main(this.get('original'),this.get('diffAttribute'),true);
+			this.set('calculatedPatch',patcher.diff_prettyHtml(diff));
+		}else{
+		    this.set('diffAttribute',this.get('original'));
+			this.set('calculatedPatch','');
+		}
     }.observes('diffAttribute'),
 	actions:{
 	    cancel: function(){
