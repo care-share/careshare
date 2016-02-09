7fcbe2435e4b2a98f8d34b8dd2afbde3ffb7bc3e
David Tresner-Kirsch <davidtk@mitre.org>
2016-02-09 16:41:09 -0500

    medrec template

--- app/router.js
+++ app/router.js
@@ -10,10 +10,10 @@ export default Router.map(function () {
     this.route('goals');
     this.resource('account', {path: '/account'});
     this.resource('workInProgress');
+    this.resource('medrec',  {path: '/patients/:patient_id/medrec'});
     this.resource('patients', function () {
         this.resource('patient', {path: '/:patient_id'}, function () {
             this.route('init');
-            this.route('medrec');
             this.route('medentry');
             this.resource('careplans', function () {
                 this.route('new');
