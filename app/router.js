5813a168aad77d792f43aa66a27d7b9a94926aef
Joe Portner <jportner@mitre.org>
2016-02-19 12:01:46 -0500

    Fix routing, refactor directory structure, add scrollbars to pages
    The old method of routing caused additional information from parent
    routes to be rendered when viewing child route pages.
    Flattened out logical routing so that this doesn't happen anymore.
    Since routing was changed, directory structure needed to be
    refactored to match.
    In addition, this now lets us add scrollbars to the 'medentry' and
    'medrec' pages.
    Added scrollbars to medentry page; medrec page needs to be
    reworked in a separate commit.

--- app/router.js
+++ app/router.js
@@ -6,26 +6,24 @@ var Router = Ember.Router.extend({
 });
 
 export default Router.map(function () {
+    // NOTE: if 'resetNamespace' is specified, the child route is not logically nested within the parent route
+    // i.e. the file structure does not need to match the nested routes of the URL
     this.route('login');
-    this.route('goals');
-    this.resource('account', {path: '/account'});
-    this.resource('workInProgress');
-    this.resource('medrec',  {path: '/patients/:patient_id/medrec'});
-    this.resource('patients', function () {
-        this.resource('patient', {path: '/:patient_id'}, function () {
-            this.route('init');
-            this.route('medentry');
-            this.resource('careplans', function () {
-                this.route('new');
-                this.resource('careplan', {path: '/:careplan_id'}, function () {
-//                    this.route('init');
-                    this.route('filters');
-                    this.route('patientInfo');
-                    this.route('notes');
-                    this.route('history');
-                    this.route('requests');
-                });
-            });
+    this.route('work-in-progress');
+    this.route('account');
+    this.route('patients');
+    this.route('patient', {path: '/patients/:patient_id'}, function () {
+        // parent route has an empty outlet
+        // this gives children access to the patient router and controller, without rendering unwanted data on the page
+        this.route('medrec', {resetNamespace: true});
+        this.route('medentry', {resetNamespace: true});
+        this.route('careplan-init', {path: '/careplans/init', resetNamespace: true});
+        this.route('careplan', {path: '/careplans/:careplan_id', resetNamespace: true}, function () {
+            this.route('filters');
+            this.route('patientInfo');
+            this.route('notes');
+            this.route('history');
+            this.route('requests');
         });
     });
 });
--- app/routes/medrec.js
+++ app/routes/medrec.js
@@ -2,8 +2,12 @@ import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
-    model: function (params) {
-        return this.store.query('medpair', {patient_id: params.patient_id});
+    model: function (/*params*/) {
+        // we can't access parameters of parent routes directly to find the patient_id
+        // instead, access the model provided by the parent route
+        var patient_id = this.modelFor('patient').id;
+        // now, query the CareAuth API for medrecs for this patient!
+        return this.store.query('medpair', {patient_id: patient_id});
     },
     actions: {}
 });
