104a54a61e388414a232542639c1cd4fb8da1e29
Joe Portner <jportner@mitre.org>
2016-02-19 19:17:18 -0500

    Fixed up MedRec page
    Page layout is now sized properly.
    Now has a 'submit' button with proper validation and error
    handling.
    Now handles cases where MedPairs don't have an ehrMed defined.
    Began implementing filters but they don't quite work yet.

--- app/controllers/medrec.js
+++ app/controllers/medrec.js
@@ -1,18 +1,97 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
-    actions: {
-        enforceVA: function (medpair) {
-            console.log("Enforcing VA")
-            // medpair.homeMed.action = 'Enforce VA';
-            medpair.set('homeMed.action', 'Enforce VA')
-        },
-        acceptHH: function (medpair) {
-            console.log("Accepting HH")
-            // medpair.homeMed.action = 'Accept HH';
-            medpair.set('homeMed.action', 'Accept HH')
+    showEnforceVA: true,
+    showAcceptHH: true,
+    showClarify: true,
+    showRenew: true,
+    showDoNotUse: true,
+    filteredContent: function () {
+        // TODO: fix this, it doesn't recompute the property when actions are changed
+        // problem is, we can change actions two ways: click on text, or click on radio button
+        // radio button doesn't allow us to specify an action that bubbles up
+        var filters = [];
+        if (this.get('showEnforceVA')) {
+            filters.addObject('Enforce VA');
         }
+        if (this.get('showAcceptHH')) {
+            filters.addObject('Accept HH');
+        }
+        if (this.get('showClarify')) {
+            filters.addObject('Clarify');
+        }
+        if (this.get('showRenew')) {
+            filters.addObject('Renew');
+        }
+        if (this.get('showDoNotUse')) {
+            filters.addObject('Do not use');
+        }
+        return this.get('model').filter(function (item) {
+            return filters.contains(item.get('homeMed.action'));
+        }, this);
+    }.property('showEnforceVA', 'showAcceptHH', 'showClarify', 'showRenew', 'showDoNotUse'),
+    actions: {
+        setAction: function (medPair, action) {
+            medPair.set('homeMed.action', action);
+        },
+        submitForm: function () {
+            // first, clear all error/success messages
+            this.clearMessages();
+
+            // validate signature and form entries
+            if (this.validateEntries() && this.validateSignature()) {
+                // everything is valid, try to save all of the medentry
+                this.saveEntries();
+            }
+        }
+    },
+    clearMessages() {
+        this.set('errorMessage', '');
+        this.set('successMessage', '');
+    },
+    validateEntries() {
+        // only return true if every MedEntry has an action selected
+        var medPairs = this.get('model').toArray();
+        if (medPairs.length === 0) {
+            this.set('errorMessage', 'There are no entries to submit!');
+            return false;
+        }
+        for (var i = 0; i < medPairs.length; i++) {
+            if (Ember.isEmpty(medPairs[i].get('homeMed.action'))) {
+                this.set('errorMessage', 'You must first select an action for all entries!');
+                return false;
+            }
+        }
+        return true;
+    },
+    validateSignature() {
+        // only return true if the user has entered a signature
+        var value = this.get('signatureText');
+        if (Ember.isEmpty(value)) {
+            this.set('errorMessage', 'You must first enter your signature!');
+            return false;
+        }
+        return true;
+    },
+    saveEntries() {
+        // prevent user from hitting submit more than once
+        this.set('disableSubmit', true);
+        // loop through medpairs, save them all, and create success message
+        var medPairs = this.get('model').toArray();
+        var promises = [];
+        for (var i = 0; i < medPairs.length; i++) {
+            var medEntry = medPairs[i].get('homeMed.content');
+            var promise = medEntry.save();
+            promises.pushObject(promise);
+        }
+        // try to save all models, and if any of them fail, throw an error
+        var that = this;
+        Ember.RSVP.all(promises).then(function(){
+            that.set('successMessage', 'Form submitted successfully!');
+        }, function(error) {
+            that.set('errorMessage', 'Form submission failed! ' + error.message);
+        }).then(function () {
+            that.set('disableSubmit', false);
+        });
     }
-
-
 });
--- app/models/medentry.js
+++ app/models/medentry.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
     _id: DS.attr('string'),
@@ -7,20 +8,22 @@ export default DS.Model.extend({
     dose: DS.attr('string'),
     freq: DS.attr('string'),
     note: DS.attr('string'),
+    not_found: DS.attr('boolean'),
     // added by user in Ember template
     action: DS.attr('string'),
     hhNotes: DS.attr('string'),
     vaNotes: DS.attr('string'),
-    actionNeedsSignature: Ember.computed('action', function() {
-        switch(this.get("action")){
-        case "Accept HH":
-        case "Enforce VA":
-        case "Clarify":
-            return true;
-            break;
-        case "No Action":
-        default:
-            return false;
+    actionNeedsSignature: Ember.computed('action', function () {
+        switch (this.get('action')) {
+            case 'Accept HH':
+            case 'Renew':
+            case 'Enforce VA':
+            case 'Do not use':
+            case 'Clarify':
+                return true;
+            default:
+            case 'No action':
+                return false;
         }
     })
 });
