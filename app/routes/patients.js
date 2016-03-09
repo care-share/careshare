d09e2902ed5ee50ba975297ce9c8b5734d3df874
Joe Portner <jportner@mitre.org>
2016-03-09 13:54:37 -0500

    Added queries for comms for 'patients' and 'careplan' pages

--- app/routes/patients.js
+++ app/routes/patients.js
@@ -10,5 +10,10 @@ export default base.extend({
                 function () {
                     console.log('PROMISE ERROR!');
                 });
+    },
+    afterModel(model) {
+        var patientIds = model.mapBy('id');
+        // find all comms for these patient IDs
+        return this.store.query('comm', {patient_id: patientIds});
     }
 });
