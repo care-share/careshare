b98fa9b0ea563d0c8416359b90a5b124622b931a
David Tresner-Kirsch <davidtk@mitre.org>
2016-05-05 13:56:42 -0400

    get more info from MedEntry into the MedRec interface

--- app/models/medentry.js
+++ app/models/medentry.js
@@ -9,6 +9,11 @@ export default DS.Model.extend({
     freq: DS.attr('string'),
     note: DS.attr('string'),
     not_found: DS.attr('boolean'),
+    compliance_bool: DS.attr('boolean'),
+    noncompliance_note: DS.attr('string'), // goes with compliance_bool:false
+    med_bool: DS.attr('boolean'), // ridiculously named variable -- true means this came from the primary care org, false means it's an outside prescriber
+    prescribed_by_primary: Ember.computed.alias('med_bool'), // so let's use another name for it 
+    prescriber_note: DS.attr('string'), // goes with med_bool/prescribed_by_primary:false
     // added by user in Ember template
     action: DS.attr('string'),
     hhNotes: DS.attr('string'),
