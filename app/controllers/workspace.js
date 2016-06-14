0c748ec589d88daed2962c74d0a6a2eb74a056c4
David Tresner-Kirsch <davidtk@mitre.org>
2016-06-14 14:34:00 -0400

    horizontal list of three resources in workspace

--- app/controllers/workspace.js
+++ app/controllers/workspace.js
@@ -6,6 +6,10 @@ export default CarePlanResource.extend({
     needs: 'careplan',
     careplan: Ember.computed.alias('controllers.careplan'),
     resources: [],
+    isFull: Ember.computed('resources', function() {
+        // This is not working -- this.get('resources').length always returns 0. why???
+        return (this.get('resources').length >= 3);
+    }),
     actions: {
         addToWorkspace: function (draggedObject/*, options*/) {
             this.get('resources').addObject(draggedObject);
