95765ace06263974d8a54cec9be8e751e27a7eae
David Tresner-Kirsch <davidtk@mitre.org>
2016-06-23 03:14:30 -0400

    add patient address to patient info

--- app/controllers/careplan/patient-info.js
+++ app/controllers/careplan/patient-info.js
@@ -4,6 +4,7 @@ export default Ember.Controller.extend({
     session: Ember.inject.service('session'),
     patient: Ember.inject.controller('patient'),
     careplan: Ember.inject.controller('careplan'),
+    address: Ember.computed.alias('patient.model.address.firstObject'),
     actions: {
         createMessage: function (message, resourceId, resourceType) {
             this.get('careplan').send('createMessage', message, resourceId, resourceType);
