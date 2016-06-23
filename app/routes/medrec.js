efa62426022ef3f8dfc440fb5c48825633fd7e89
Joe Portner <jportner@mitre.org>
2016-06-23 12:05:55 -0400

    Fixed typo in recent refactor that broke medrec route

--- app/routes/medrec.js
+++ app/routes/medrec.js
@@ -20,7 +20,7 @@ export default base.extend({
     model: function (/*params*/) {
         // we can't access parameters of parent routes directly to find the patient_id
         // instead, access the model provided by the parent route
-        var patient_id = this.modelFor('patient').model.id;
+        var patient_id = this.modelFor('patient').id;
         // now, query the CareAuth API for medrecs for this patient!
         return this.store.query('medpair', {patient_id: patient_id});
     },
